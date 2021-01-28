637bc74a7d9b13c7f230f9be41d0ef3bb768ab1c
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-28 16:58:41 -0300

    Primeiro commit

---
+++ src/main/java/desafio/alfrest/api/AlfRestApiApplication.java
@@ -0,0 +1,13 @@
+package desafio.alfrest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/alfrest/api/AlfRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.alfrest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
