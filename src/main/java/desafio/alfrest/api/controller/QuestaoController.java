fc26a3170ed8cb8a9aa1d9a76ecf31a645f39c58
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-30 17:10:38 -0300

    Adicionado todos metodos @Post e @Get relacionado as repostas do aluno e o gabarito.

--- src/main/java/desafio/alfrest/api/controller/QuestaoController.java
+++ src/main/java/desafio/alfrest/api/controller/QuestaoController.java
@@ -1,14 +1,15 @@
 package desafio.alfrest.api.controller;
 
-import desafio.alfrest.api.controller.DTO.ProvaRs;
 import desafio.alfrest.api.controller.DTO.QuestaoRq;
 import desafio.alfrest.api.controller.DTO.QuestaoRs;
 import desafio.alfrest.api.controller.repository.QuestaoRepository;
 import desafio.alfrest.api.model.Questao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,31 +20,147 @@ public class QuestaoController {
     @Autowired
     private QuestaoRepository repository;
 
-    // Consulta e retorna as questoes que nao sao de gabarito:
-    @GetMapping(path = "/questoes")
+    // ====== QUESTOES ======
+
+    // Consulta e retorna todas as questoes cadastradas:
+    @GetMapping(path = "/questao")
     public List<QuestaoRs> consultarQuestoes() {
-        var questoes = this.repository.findAll();
+        var questoes = this.repository.findQuestoes();
         return questoes
                 .stream().map(questao -> QuestaoRs.converter(questao))
                 .collect(Collectors.toList());
     }
 
-    // Faz uam consulta pelo id da questao:
-    @GetMapping(path = "/questao/{id}")
-    public QuestaoRs consulta(@PathVariable("id") Integer id) {
-        var questao = this.repository.getOne(id);
-        return QuestaoRs.converter(questao);
+    // Consulta e retorna as questôes pelo id da prova:
+    @GetMapping(path = "/questao/prova/{id_prova}")
+    public List<QuestaoRs> consultarQuestaoIdProva(@PathVariable("id_prova") Integer id_prova) {
+        var questoes = this.repository.findByIdProva(id_prova);
+        return questoes
+                .stream().map(questao -> QuestaoRs.converter(questao))
+                .collect(Collectors.toList());
     }
 
-    // Cadastra uma nova questão:
-    @PostMapping(path = "/questao/cadastrar")
-    public QuestaoRs cadastrar(@RequestBody QuestaoRq questao) {
+    // Cadastra uma nova questão para a prova pelo id:
+    @PostMapping(path = "/questao/prova")
+    public QuestaoRs cadastrarQuestao(
+            @RequestBody QuestaoRq questao,
+            @RequestParam(value = "id", required = false, defaultValue = "0") Integer id_prova) {
         var q = new Questao();
+        q.setId(null);
         q.setAlternativa(questao.getAlternativa());
         q.setPeso(questao.getPeso());
-        q.setId_prova(questao.getId_prova());
         q.setGabarito(false);
+
+        // Define id_prova == 0 se ele não for informado no RequestBody
+        if (questao.getId_prova() == null)
+            questao.setId_prova(0);
+
+        if (id_prova != questao.getId_prova() && id_prova == 0) {
+            // se o parametro for 0 e o id não
+            q.setId_prova(questao.getId_prova());
+        } else {
+            if (id_prova != questao.getId_prova() && questao.getId_prova() == 0) {
+                // se o id for 0 e o parametro não
+                q.setId_prova(id_prova);
+            } else {
+                if( id_prova == questao.getId_prova() && id_prova != 0) {
+                    q.setId_prova(id_prova);
+                } else {
+                    q.setId_prova(null);
+                }
+            }
+        }
         return QuestaoRs.converter(this.repository.save(q));
+    }
+
+    // Cadastra uma lista de questões:
+    @PostMapping(path = "/questao/lista/{id_prova}",  consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Transactional
+    public List<QuestaoRs> cadastrarQuestoes(@RequestBody List<QuestaoRs> questoes) {
+        List<QuestaoRs> result = new ArrayList<>();
+        // percorre a lista de questões recebida:
+        for (QuestaoRs q : questoes) {
+            var questao = new Questao();
+            questao.setId(null);
+            questao.setId_prova(q.getId_prova());
+            questao.setAlternativa(q.getAlternativa());
+            questao.setPeso(q.getPeso());
+            questao.setGabarito(false);
+            result.add(QuestaoRs.converter(this.repository.save(questao)));
+        }
+        return result;
+    }
+
+    // ====== GABARITO ======
+
+    // Consulta e retorna todas as questoes que nao sao de gabarito:
+    @GetMapping(path = "/gabarito")
+    public List<QuestaoRs> consultarGabaritos() {
+        var questoes = this.repository.findGabaritos();
+        return questoes
+                .stream().map(questao -> QuestaoRs.converter(questao))
+                .collect(Collectors.toList());
+    }
+
+    // Faz uma consulta pelo id da prova:
+    @GetMapping(path = "/gabarito/prova/{id_prova}")
+    public List<QuestaoRs> consultarGabaritoIdProva(@PathVariable("id_prova") Integer id_prova) {
+        var questoes = this.repository.findGabaritoByIdProva(id_prova);
+        return questoes
+                .stream().map(questao -> QuestaoRs.converter(questao))
+                .collect(Collectors.toList());
+    }
+
+
+    // Cadastra uma nova resposta para o gabarito pelo id:
+    @PostMapping(path = "/gabarito/resposta/prova")
+    public QuestaoRs cadastrarQuestaoGabarito(
+            @RequestBody QuestaoRq questao,
+            @RequestParam(value = "id", required = false, defaultValue = "0") Integer id_prova) {
+        var q = new Questao();
+        q.setId(null);
+        q.setAlternativa(questao.getAlternativa());
+        q.setPeso(questao.getPeso());
+        q.setGabarito(true);
+
+        // Define id_prova == 0 se ele não for informado no RequestBody
+        if (questao.getId_prova() == null)
+            questao.setId_prova(0);
+
+        if (id_prova != questao.getId_prova() && id_prova == 0) {
+            // se o parametro for 0 e o id não
+            q.setId_prova(questao.getId_prova());
+        } else {
+            if (id_prova != questao.getId_prova() && questao.getId_prova() == 0) {
+                // se o id for 0 e o parametro não
+                q.setId_prova(id_prova);
+            } else {
+                if( id_prova == questao.getId_prova() && id_prova != 0) {
+                    q.setId_prova(id_prova);
+                } else {
+                    q.setId_prova(null);
+                }
+            }
+        }
+        return QuestaoRs.converter(this.repository.save(q));
+    }
+
+    // Cadastra uma lista de questões gabarito:
+    @PostMapping(path = "/gabarito/resposta/lista/{id_prova}",  consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Transactional
+    public List<QuestaoRs> cadastrarQuestoesGabarito(@RequestBody List<QuestaoRs> questoes) {
+        List<QuestaoRs> result = new ArrayList<>();
+        // percorre a lista de questões recebida:
+        for (QuestaoRs q : questoes) {
+            var questao = new Questao();
+            questao.setId(null);
+            questao.setId_prova(q.getId_prova());
+            questao.setAlternativa(q.getAlternativa());
+            questao.setPeso(q.getPeso());
+            questao.setGabarito(true);
+            result.add(QuestaoRs.converter(this.repository.save(questao)));
+        }
+        return result;
     }
 
 }
--- src/main/java/desafio/alfrest/api/controller/repository/QuestaoRepository.java
+++ src/main/java/desafio/alfrest/api/controller/repository/QuestaoRepository.java
@@ -2,6 +2,29 @@ package desafio.alfrest.api.controller.repository;
 
 import desafio.alfrest.api.model.Questao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Integer> {
+
+    // Retorna todas as respostas cadastradas
+    @Query(value = "SELECT * FROM questao q WHERE q.gabarito = false ORDER BY ID", nativeQuery = true)
+    List<Questao> findQuestoes();
+
+    // Retorna todos os gabaritos cadastrados:
+    @Query(value = "SELECT * FROM questao q WHERE q.gabarito = true ORDER BY ID", nativeQuery = true)
+    List<Questao> findGabaritos();
+
+    // Retorna todas as respostas de uma prova pelo id:
+    @Query(value = "SELECT * FROM questao q WHERE q.id_prova = :idprova AND q.gabarito = false", nativeQuery = true)
+    List<Questao> findByIdProva(@Param("idprova") Integer id);
+
+    // Retorna o gabarito de uma prova pelo id
+    @Query(value = "SELECT * FROM questao q WHERE q.id_prova = :idprova AND q.gabarito = true", nativeQuery = true)
+    List<Questao> findGabaritoByIdProva(@Param("idprova") Integer id);
+
 }
