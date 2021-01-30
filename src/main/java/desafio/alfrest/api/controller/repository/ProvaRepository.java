49535a09d83727cbf405a5dc71aaae93b13d4733
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-30 20:51:40 -0300

    Adicionado método para consulta do aluno pelo id;

--- src/main/java/desafio/alfrest/api/controller/repository/ProvaRepository.java
+++ src/main/java/desafio/alfrest/api/controller/repository/ProvaRepository.java
@@ -2,6 +2,15 @@ package desafio.alfrest.api.controller.repository;
 
 import desafio.alfrest.api.model.Prova;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProvaRepository extends JpaRepository<Prova, Integer> {
+
+    // Retorna todas as respostas de uma prova pelo id:
+    @Query(value = "SELECT * FROM prova a WHERE a.id_aluno = :idaluno", nativeQuery = true)
+    List<Prova> findProvaByIdAluno(@Param("idaluno") Integer id);
+
 }
