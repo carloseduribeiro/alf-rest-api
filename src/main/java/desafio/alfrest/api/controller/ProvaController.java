c247c7748383e84beeb96ab83ab434de6cc960a1
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-30 18:37:24 -0300

    Agora o método consultarProvas retorna as questões quando o atributo "questoes" na url for true;

--- src/main/java/desafio/alfrest/api/controller/ProvaController.java
+++ src/main/java/desafio/alfrest/api/controller/ProvaController.java
@@ -1,13 +1,12 @@
 package desafio.alfrest.api.controller;
 
-import desafio.alfrest.api.controller.DTO.AlunoRq;
-import desafio.alfrest.api.controller.DTO.AlunoRs;
 import desafio.alfrest.api.controller.DTO.ProvaRq;
 import desafio.alfrest.api.controller.DTO.ProvaRs;
+import desafio.alfrest.api.controller.DTO.QuestaoRs;
 import desafio.alfrest.api.controller.repository.ProvaRepository;
+import desafio.alfrest.api.controller.repository.QuestaoRepository;
 import desafio.alfrest.api.model.Prova;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -22,21 +21,42 @@ public class ProvaController {
     @Autowired
     private ProvaRepository repository;
 
+    @Autowired
+    private QuestaoRepository questaoRepository;
 
     // Consulta e retorna todas as provas:
     @GetMapping(path = "/provas")
     public List<ProvaRs> consultarProvas(@RequestParam(value = "questoes", required = false, defaultValue = "false") Boolean val) {
         var provas = this.repository.findAll();
+        List<ProvaRs> result = new ArrayList<>();
+
         if (!val) {
             // caso a propriedade não seja informada ou seja igual a false:
             // Retorna uma lista das provas cadastradas no banco.
-            return provas
+            result = provas
                     .stream().map(prova -> ProvaRs.converter(prova))
                     .collect(Collectors.toList());
         } else {
-            // Retorna uma lista das provas e suas questões:
-            return new ArrayList<>(null);
+            // Consulta e cria uma lista das provas cadastradas no banco:
+            result = provas
+                    .stream().map(prova -> ProvaRs.converter(prova))
+                    .collect(Collectors.toList());
+
+            // Percorre e adiciona a lista de questôes ao resultado:
+            for( ProvaRs prova : result ) {
+                List<QuestaoRs> questoes = new ArrayList<>();   // armazena a lista das questões.
+
+                // Consulta e armazena a lista das questões:
+                questoes = questaoRepository.findGabaritoByIdProva(prova.getId())
+                        .stream()
+                        .map(questao -> QuestaoRs.converter(questao))
+                        .collect(Collectors.toList());
+                // Atribui a lista de questão ao objeto prova:
+                prova.setQuestoes(questoes);
+            }
         }
+        // Retorna o resultado:
+        return result;
     }
 
     // Faz uma consulta pelo id da prova:
