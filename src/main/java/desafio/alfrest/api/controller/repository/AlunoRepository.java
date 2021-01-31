2a8375856a5d431d3e0330b8482a71b3f0a00b56
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-30 22:29:24 -0300

    Adicionado a query para retornar os alunos aprovados.

--- src/main/java/desafio/alfrest/api/controller/repository/AlunoRepository.java
+++ src/main/java/desafio/alfrest/api/controller/repository/AlunoRepository.java
@@ -2,6 +2,13 @@ package desafio.alfrest.api.controller.repository;
 
 import desafio.alfrest.api.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+
+    // Retorna todas as respostas de uma prova pelo id:
+    @Query(value = "SELECT * FROM aluno a WHERE a.situacao = true ORDER BY ID", nativeQuery = true)
+    List<Aluno> findAlunosAprovados();
 }
