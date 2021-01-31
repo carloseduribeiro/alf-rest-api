030ca85eb5afb866e4fe14759b542a148e029bad
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-01-30 22:28:13 -0300

    Adicionado método estático para retornar as repostas do aluno, gabarito e pesos das questões;

--- src/main/java/desafio/alfrest/api/controller/repository/ProvaRepository.java
+++ src/main/java/desafio/alfrest/api/controller/repository/ProvaRepository.java
@@ -13,4 +13,17 @@ public interface ProvaRepository extends JpaRepository<Prova, Integer> {
     @Query(value = "SELECT * FROM prova a WHERE a.id_aluno = :idaluno", nativeQuery = true)
     List<Prova> findProvaByIdAluno(@Param("idaluno") Integer id);
 
+    // Retorna uma lista com as respostas da prova do aluno:
+    @Query(value = "SELECT alternativa FROM questao q WHERE q.id = :idprova AND q.gabarito = false ORDER BY ID", nativeQuery = true)
+    List<String> findRespostasByIdProva(@Param("idprova") Integer id);
+
+    // Retorna uma lista com as respostas do gabarito da prova:
+    @Query(value = "SELECT alternativa FROM questao q WHERE q.id = :idprova AND q.gabarito = true ORDER BY ID", nativeQuery = true)
+    List<String> findGabaritoByIdProva(@Param("idprova") Integer id);
+
+    // Retorna uma lista com o peso de cada questao:
+    @Query(value = "SELECT peso FROM questao q WHERE q.id = :idprova AND q.gabarito = true", nativeQuery = true)
+    List<Float> findQuestaoPesoByIdProva(@Param("idprova") Integer id);
+
+
 }
