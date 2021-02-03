586f2c7db8c5b6adfbd43cce4e8dc7adafa40f9b
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-02-02 22:39:54 -0300

    Adicionado metodos save e findAll();

--- src/main/java/desafio/alfrest/api/controller/AlunoController.java
+++ src/main/java/desafio/alfrest/api/controller/AlunoController.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(path = "/api/alunos")
 public class AlunoController {
@@ -17,26 +21,30 @@ public class AlunoController {
 
     // Faz uma consulta pelo id do aluno:
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Aluno> consultar(@PathVariable("id") Integer id) {
+    public ResponseEntity<AlunoResponse> consultar(@PathVariable("id") Integer id) {
         // Faz e retorna a consulta:
-        Aluno aluno = this.alunoService.findById(id);
+        AlunoResponse aluno = AlunoResponse.converter(this.alunoService.findById(id));
         return ResponseEntity.ok().body(aluno);
     }
 
-
     // Cadastra um novo aluno:
-    @PostMapping(value = "/cadastrar")
-    public ResponseEntity<AlunoResponse> cadastrar(@RequestBody AlunoRequest alunoRq) {
+    @PostMapping
+    public ResponseEntity<AlunoResponse> cadastrar(@Valid @RequestBody AlunoRequest alunoRq) {
         Aluno aluno = this.alunoService.save(alunoRq.toAluno());
         return ResponseEntity.ok().body(AlunoResponse.converter(aluno));
     }
 
-    /*
     // Consulta e retorna todos os alunos cadastrados:
     @GetMapping
-    public List<Aluno> consultarAlunos() {
-        return this.repository.findAll();
+    public ResponseEntity<List<AlunoResponse>> consultarAlunos() {
+
+        // Pesquisa e armazena todos os alunos cadastrados:
+        List<AlunoResponse> alunos = this.alunoService
+                .findAll().stream()
+                .map(AlunoResponse::converter)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(alunos);
     }
-    */
 
 }
--- src/main/java/desafio/alfrest/api/services/AlunoService.java
+++ src/main/java/desafio/alfrest/api/services/AlunoService.java
@@ -6,17 +6,29 @@ import desafio.alfrest.api.model.Aluno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AlunoService {
 
     @Autowired
     private AlunoRepository alunoRepository;
 
-    // consulta um alunon pelo id:
+    // consulta um aluno pelo id:
     public Aluno findById(Integer id){
         return alunoRepository.findById(id)
                 // Caso o id não exista dispara a exception personalisada:
-                .orElseThrow(() -> new EntityNotFoundException("Id do aluno não encontrada: " + id));
+                .orElseThrow(() -> new EntityNotFoundException("Aluno não cadastrado. ID: " + id));
     }
 
+    // cadastra um novo aluno:
+    public Aluno save(Aluno aluno) {
+        aluno.setId(null);
+        return this.alunoRepository.save(aluno);
+    }
+
+    // consulta todos os alunos no banco e retorna em uma lista:
+    public List<Aluno> findAll() {
+        return this.alunoRepository.findAll();
+    }
 }
