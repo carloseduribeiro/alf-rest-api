70e2addaebaaf66796692e9582d447dc3c22eb06
Carlos Eduardo Ribeiro <carloseribeiro96@gmail.com>
2021-02-02 22:38:56 -0300

    Adicionado exceção MethodArgumentNotValidException.

--- src/main/java/desafio/alfrest/api/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/desafio/alfrest/api/controller/exceptions/ControllerExceptionHandler.java
@@ -1,26 +1,53 @@
 package desafio.alfrest.api.controller.exceptions;
 
 import desafio.alfrest.api.services.exceptions.EntityNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    // Retorna uma resposta com o body do StandardError:
+    // Dispara quando uma entidade não é encontrada no DB:
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
         StandardError erro = new StandardError();
+
         erro.setTimestamp(Instant.now());
         erro.setStatus(HttpStatus.NOT_FOUND.value());
-        erro.setError("Recurso não encontrado.");
-        erro.setMessage(e.getMessage());
         erro.setPath(request.getRequestURI());
+        erro.setMessage("Recurso não encontrado.");
+        erro.setErrors(List.of(e.getMessage()));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    // Dispara quando um campo de formulário não é preenhido é inválido.
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> argumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        StandardError erro = new StandardError();
+
+        erro.setTimestamp(Instant.now());
+        erro.setPath(request.getRequestURI());
+        erro.setStatus(HttpStatus.NOT_FOUND.value());
+//        erro.setMessage(e.getMessage());
+        erro.setMessage("Algun(s) campos devem ser prenchido(s).");
+        erro.setErrors(
+                e.getBindingResult()
+                        .getFieldErrors()
+                        .stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.toList())
+        );
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
